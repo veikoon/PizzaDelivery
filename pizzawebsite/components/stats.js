8afeadab76e280e8166855f3139d639937f8a512
ialexvo <alex.vo@edu.esiee.fr>
2021-06-20 16:20:19 +0200

    Add staff stats

--- pizzawebsite/components/stats.js
+++ pizzawebsite/components/stats.js
@@ -1,36 +1,13 @@
-Vue.component('stat', {
-    props: ['stats'],
-    template: `
-    <div class="statcomponent">
-        <div class = "groupe">
-            {{stats.groupe}}
-        </div>
-        <hr>
-        <table id = "stats">
-        <colgroup>
-        <col span="1" style="width: 100%;">
-        <col span="2" style="width: 100%;">
-        </colgroup>
-        <tbody>
-  
-        <!-- Assigning width of second 
-             column of each row as 60% -->
-        <col style="width: 60%;" />    
-        <div v-for="elem in stats.elements" v-bind:key="elem.nom" v-bind:elem='stats.elements' class="data"
-        width = 100%>
-        <tr>
-            <td>{{elem.nom}} </td>
-            <td>{{elem.data}} </td>
-        </tr>
-        </div>
-        
-        </tbody>
-        </table>
-        <br>
+Vue.component('statscomp', {
+    template : `
+    <div class = "groupe">
+        <p id= food_title> {{food_stats['groupename']}}  </p>
     </div>`
+    
+
 })
 
-var app = new Vue({
+/*var app = new Vue({
     el: '#statistiques',
     data: {
         stats: [{
@@ -89,5 +66,58 @@ var app = new Vue({
                 }]
             }
         ]
+    }
+})
+*/
+
+var app = new Vue({
+    el: '#statistiques',
+    data: {
+        food_stats :
+            {
+                group_name : "Nourriture",
+                best_pizza : "Marguerita",
+                worst_pizza : "Hawaienne",
+                favorite_ingredient : "Pepperoni"
+            },
+        staff_stats :
+            {
+                group_name : "Employes",
+                Pire_employe : {
+                    staff_name : "Cathy",
+                    staff_surname : "Truong",
+                    staff_nb_delay : 69
+                }
+            }/*,
+        top_clients_stats : {
+            group_name : "Top 5 clients",
+        },
+        unused_vehicles_stats : {
+            group_name : "Vehicules n'ayant jamais servis",
+        }*/
+    },
+
+    // Looking for best employee
+    mounted: function() {
+        this.$nextTick(function() {
+            const fetchpromise = fetch("http://localhost:8080/staff/all");
+            fetchpromise.then(response => {
+                return response.json();
+            }).then(staff => {
+                min_delay = 0;
+                staff_index;
+                for (let i = 0; i < staff.length; i++) {
+                    console.log(i);
+                    if(staff[i].nb_delay <= min_delay) {
+                        min_delay = staff[i].nb_delay;
+                        staff_index = i;
+                    }
+                }
+                this.stats["staff"]["best_staff"] = {
+                    staff_name: staff[staff_index].name,
+                    staff_surname: staff[staff_index].surname
+                }
+            });
+        })
     }
 })
--- pizzawebsite/scripts/statdata.js
+++ pizzawebsite/scripts/statdata.js
@@ -28,35 +28,36 @@ fetchpromise = fetch("http://localhost:8080/staff/all");
 fetchpromise.then(response => {
     return response.json();
 }).then(staff => {
-
+    console.log(staff.length);
     for (let i = 0; i < staff.length; i++) {
-        console.log(i);
-        if (staff[i].nb_delay < min_delay) {
-            min_delay = staff[i].nb_delay;
+        //console.log(i);
+        if (staff[i].nbDelay < min_delay) {
+            min_delay = staff[i].nbDelay;
             best_staff_index = i;
         }
-        if (staff[i].nb_delay > max_delay) {
-            max_delay = staff[i].nb_delay;
+        if (staff[i].nbDelay > max_delay) {
+            max_delay = staff[i].nbDelay;
             worst_staff_index = i;
         }
+        console.log(max_delay);
     }
     stats.staff_stats.best_staff = {
         staff_name: staff[best_staff_index].name,
         staff_surname: staff[best_staff_index].surname,
-        staff_nb_delay: staff[best_staff_index].nb_delay
+        staff_nb_delay: staff[best_staff_index].nbDelay
     }
 
     stats.staff_stats.worst_staff = {
         staff_name: staff[worst_staff_index].name,
         staff_surname: staff[worst_staff_index].surname,
-        staff_nb_delay: staff[worst_staff_index].nb_delay
+        staff_nb_delay: staff[worst_staff_index].nbDelay
     }
     console.log(stats.staff_stats.best_staff.staff_name);
     console.log(stats.staff_stats.worst_staff);
     document.getElementById('food_stats_title').innerHTML = stats.food_stats.group_name;
     document.getElementById('staff_stats_title').innerHTML = stats.staff_stats.group_name;
-    document.getElementById('best_staff').innerHTML = stats.staff_stats.best_staff.staff_name + " " + stats.staff_stats.best_staff.staff_surname;
-    document.getElementById('worst_staff').innerHTML = stats.staff_stats.worst_staff.staff_name + " " + stats.staff_stats.worst_staff.staff_surname;
+    document.getElementById('best_staff').innerHTML += stats.staff_stats.best_staff.staff_name + " " + stats.staff_stats.best_staff.staff_surname + " - " + stats.staff_stats.best_staff.staff_nb_delay + " retards";
+    document.getElementById('worst_staff').innerHTML += stats.staff_stats.worst_staff.staff_name + " " + stats.staff_stats.worst_staff.staff_surname + " - " + stats.staff_stats.worst_staff.staff_nb_delay + " retards";;
 });
 
 //document.getElementById('top_clients_title').innerHTML = data["client_stats"]["group_name"];
