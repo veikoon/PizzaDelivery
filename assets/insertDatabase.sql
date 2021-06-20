38909e87f6bf0f7f0bc031efd4c163a5946073bd
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2021-06-20 22:11:06 +0200

    add insert

---
+++ assets/insertDatabase.sql
@@ -0,0 +1,15 @@
+INSERT INTO `client` VALUES (1,'3 rue des Colmarts Arnouville',150,'Benjamin',3,'0645897814','Pavard',15),(2,'4 rue du Michelin St-Leu',56,'Karim',1,'0745639810','Benzema',20);
+
+INSERT INTO `ingredient` VALUES (5,'Ananas'),(3,'Mozzarella'),(1,'Pepperoni'),(4,'Poivron'),(2,'Tomate');
+
+INSERT INTO `pizza` VALUES (5,'Hawaiienne'),(6,'Marguerita'),(4,'Pepperoni');
+
+INSERT INTO `pizza_ingredients` VALUES (4,1),(4,3),(5,5),(5,3),(6,3);
+
+INSERT INTO `size` VALUES (1,'Naine',5),(2,'Humaine',13),(3,'Ogresse',20);
+
+INSERT INTO `staff` VALUES (1,'19 rue Jean Bonhomme Paris',18,'Monnot',0,'0645789863','Vincent'),(2,'19 rue Hector Solide Creteil',25,'Vo',0,'0613803193','Alex'),(3,'19 rue Elise Chatouille Marseille',48,'Truong',0,'0645789635','Cathy');
+
+INSERT INTO `vehicule` VALUES (1,'Scooter'),(2,'Voiture');
+
+INSERT INTO `delivery` VALUES (1,'2021-06-20 19:15:13',0,1,4,3,1,1),(2,'2021-06-20 19:15:39',0,2,5,1,3,2),(3,'2021-06-20 19:15:53',0,2,4,2,2,1);
