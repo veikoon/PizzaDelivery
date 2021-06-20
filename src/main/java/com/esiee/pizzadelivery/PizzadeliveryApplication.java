50e25995b22ab4c5c014dfe65828c325bb1cc07c
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2021-06-20 17:26:03 +0200

    uncomment swagger

--- src/main/java/com/esiee/pizzadelivery/PizzadeliveryApplication.java
+++ src/main/java/com/esiee/pizzadelivery/PizzadeliveryApplication.java
@@ -2,10 +2,10 @@ package com.esiee.pizzadelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
 public class PizzadeliveryApplication {
 
 	public static void main(String[] args) {
