99cfa71d62b8ac316b4ec3cf14db1f3e55d730f1
ktou <55832645+kathou-png@users.noreply.github.com>
2021-06-20 16:51:48 +0200

    best/worst pizza

--- pizzawebsite/scripts/statdata.js
+++ pizzawebsite/scripts/statdata.js
@@ -28,9 +28,9 @@ fetchpromise = fetch("http://localhost:8080/staff/all");
 fetchpromise.then(response => {
     return response.json();
 }).then(staff => {
-    console.log(staff.length);
+
     for (let i = 0; i < staff.length; i++) {
-        //console.log(i);
+        console.log(i);
         if (staff[i].nbDelay < min_delay) {
             min_delay = staff[i].nbDelay;
             best_staff_index = i;
@@ -39,26 +39,49 @@ fetchpromise.then(response => {
             max_delay = staff[i].nbDelay;
             worst_staff_index = i;
         }
-        console.log(max_delay);
     }
     stats.staff_stats.best_staff = {
         staff_name: staff[best_staff_index].name,
         staff_surname: staff[best_staff_index].surname,
-        staff_nb_delay: staff[best_staff_index].nbDelay
+        staff_nb_delay: staff[best_staff_index].nb_delay
     }
 
     stats.staff_stats.worst_staff = {
         staff_name: staff[worst_staff_index].name,
         staff_surname: staff[worst_staff_index].surname,
-        staff_nb_delay: staff[worst_staff_index].nbDelay
+        staff_nb_delay: staff[worst_staff_index].nb_delay
     }
     console.log(stats.staff_stats.best_staff.staff_name);
     console.log(stats.staff_stats.worst_staff);
     document.getElementById('food_stats_title').innerHTML = stats.food_stats.group_name;
     document.getElementById('staff_stats_title').innerHTML = stats.staff_stats.group_name;
-    document.getElementById('best_staff').innerHTML += stats.staff_stats.best_staff.staff_name + " " + stats.staff_stats.best_staff.staff_surname + " - " + stats.staff_stats.best_staff.staff_nb_delay + " retards";
-    document.getElementById('worst_staff').innerHTML += stats.staff_stats.worst_staff.staff_name + " " + stats.staff_stats.worst_staff.staff_surname + " - " + stats.staff_stats.worst_staff.staff_nb_delay + " retards";;
+    document.getElementById('best_staff').innerHTML = stats.staff_stats.best_staff.staff_name + " " + stats.staff_stats.best_staff.staff_surname;
+    document.getElementById('worst_staff').innerHTML = stats.staff_stats.worst_staff.staff_name + " " + stats.staff_stats.worst_staff.staff_surname;
 });
 
-//document.getElementById('top_clients_title').innerHTML = data["client_stats"]["group_name"];
-//document.getElementById('vehicle_stats_title').innerHTML = data["vehicle_stats"]["group_name"];
+let countpizzas = [];
+let changed = 0;
+fetchpromise = fetch("http://localhost:8080/delivery/all");
+fetchpromise.then(response => { return response.json(); }).then(orders => {
+    var result = {};
+    orders.forEach(function(elem) {
+        if (elem.pizza.name in result) {
+            result[elem.pizza.name] = ++result[elem.pizza.name];
+        } else {
+            result[elem.pizza.name] = 1;
+        }
+    });
+    max = orders[0].pizza.name;
+    min = orders[0].pizza.name;
+    orders.forEach(function(elem) {
+        if (result[max] < result[elem.pizza.name]) { max = elem.pizza.name; }
+        if (result[min] > result[elem.pizza.name]) {
+            console.log(result[min]);
+            console.log(elem);
+            min = elem.pizza.name;
+        }
+    });
+    console.log(max);
+    document.getElementById('best_pizza').innerHTML = max;
+    document.getElementById('worst_pizza').innerHTML = min;
+});
